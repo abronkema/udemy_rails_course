832e275b819a1d6e06e2806fdec41a7f7ad808d7
Anthony Bronkema <abronkema@customink.com>
2019-08-08 22:22:25 -0400

    corrected the key :username to :name in methods

--- oop_projects/crud.rb
+++ oop_projects/crud.rb
@@ -7,9 +7,39 @@ end
 
 require 'bcrypt'
 
+users = [
+  { name: 'Anthony', password: 'password1' },
+  { name: 'Becca', password: 'password2' },
+  { name: 'Callie', password: 'password3' },
+  { name: 'Birdie', password: 'password4' }
+]
+
 def create_hash_digest(password)
   BCrypt::Password.create(password)
 end
 
-new_password = create_hash_digest("password1")
-puts new_password == "password1"
+def verify_hash_digest(password)
+  BCrypt::Password.new(password)
+end
+
+def create_secure_credentials(list_of_users)
+  list_of_users.each do |user_record|
+    user_record[:password] = create_hash_digest(user_record[:password])
+  end
+  list_of_users
+end
+
+new_users = create_secure_credentials(users)
+puts new_users
+
+def authenticate_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:name] == username &&
+       user_record[:password] == password
+      return user_record
+    end
+  end
+  'Invalid credentials'
+end
+
+puts authenticate_user('Anthony', 'password1', new_users)
