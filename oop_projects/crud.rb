30b11f773c662750180e0eb78700011e4a0c77bf
Anthony Bronkema <abronkema@customink.com>
2019-08-09 15:22:50 -0400

    moved users array to separate module

--- oop_projects/crud.rb
+++ oop_projects/crud.rb
@@ -1,45 +1,29 @@
-require 'bundler/inline'
 
-gemfile true do
-  source 'http://rubygems.org'
-  gem 'bcrypt'
-end
-
-require 'bcrypt'
-
-users = [
-  { name: 'Anthony', password: 'password1' },
-  { name: 'Becca', password: 'password2' },
-  { name: 'Callie', password: 'password3' },
-  { name: 'Birdie', password: 'password4' }
-]
-
-def create_hash_digest(password)
-  BCrypt::Password.create(password)
-end
-
-def verify_hash_digest(password)
-  BCrypt::Password.new(password)
-end
-
-def create_secure_credentials(list_of_users)
-  list_of_users.each do |user_record|
-    user_record[:password] = create_hash_digest(user_record[:password])
+module Crud
+  require 'bcrypt'
+  puts 'Module loaded successfully.'
+  def self.create_hash_digest(password)
+    BCrypt::Password.create(password)
   end
-  list_of_users
-end
-
-new_users = create_secure_credentials(users)
-puts new_users
-
-def authenticate_user(username, password, list_of_users)
-  list_of_users.each do |user_record|
-    if user_record[:name] == username &&
-       user_record[:password] == password
-      return user_record
+  
+  def verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+  
+  def self.create_secure_credentials(list_of_users)
+    list_of_users.each do |user_record|
+      user_record[:password] = create_hash_digest(user_record[:password])
     end
+    list_of_users
   end
-  'Invalid credentials'
+  
+  def authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      if user_record[:name] == username &&
+        user_record[:password] == password
+        return user_record
+      end
+    end
+    'Invalid credentials'
+  end
 end
-
-puts authenticate_user('Anthony', 'password1', new_users)
--- oop_projects/main.rb
+++ oop_projects/main.rb
@@ -1,30 +1,11 @@
-require 'bundler/inline'
+require_relative 'crud'
 
-gemfile true do
-  source 'http://rubygems.org'
-  gem 'bcrypt'
-end
+users = [
+  { name: 'Anthony', password: 'password1' },
+  { name: 'Becca', password: 'password2' },
+  { name: 'Callie', password: 'password3' },
+  { name: 'Birdie', password: 'password4' }
+]
 
-require 'bcrypt'
-
-my_password = BCrypt::Password.create("test")
-my_password2 = BCrypt::Password.create("test2")
-my_password3 = BCrypt::Password.create("test3")
-  #=> "$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa"
-
-puts my_password
-puts my_password2
-puts my_password3
-
-# puts my_password.version              #=> "2a"
-# puts my_password.cost                 #=> 10
-# puts my_password == "my password"     #=> true
-# puts my_password == "not my password" #=> false
-
-my_password = BCrypt::Password.new("$2a$12$IZUgYX3gmsVSrYhnTgFfjuw1k1d2Gh.NgZLIADOfQZMRtIetLaxM.")
-my_password2 = BCrypt::Password.new("$2a$12$BydFq8.xvwM00qWYoC9z9.6yAXwzIIDNC5K9hVDEfmOigA5GkFITm")
-my_password3 = BCrypt::Password.new("$2a$12$n/bSWO8b06KqCfRrCMvmy.CzwbobWwtyvHZDmLscfr8eBetP/0tZ.")
-puts my_password == "test"     #=> true
-puts my_password2 == "test2"     #=> true
-puts my_password3 == "test3"     #=> true
-# puts my_password == "not my password" #=> false
+hashed_users = Crud.create_secure_credentials(users)
+puts hashed_users
